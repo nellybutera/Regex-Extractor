1feba0a6b5921f5886384968ad8195f88b32f6bf
nellybutera <buteranelly250@gmail.com>
2025-07-08 04:22:50 +0200

    project setup completed

---
+++ src/main/java/com/example/Regex/Extractor/RegexExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Regex.Extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegexExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Regex/Extractor/RegexExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Regex.Extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
