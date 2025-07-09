640965ff5d3eba1d995fcf03106155898343e156
nellybutera <buteranelly250@gmail.com>
2025-07-09 04:29:21 +0200

    added exception handler code

---
+++ src/main/java/com/example/Regex/Extractor/controller/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.regexextractor.controller;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralError(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+    }
+}
